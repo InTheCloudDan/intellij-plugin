9849d09f1dfe7be67dfc176152b154d97ade32cc
Dan O'Brien <dobrien@launchdarkly.com>
2020-05-22 16:27:58 -0400

    window fixes

--- src/main/kotlin/com/launchdarkly/FeatureFlagWindow.kt
+++ src/main/kotlin/com/launchdarkly/FeatureFlagWindow.kt
@@ -12,35 +12,36 @@ import javax.swing.JPanel
 
 
 class FeatureFlagWindow(toolWindow: ToolWindow) {
-    private val refreshToolWindowButton: JButton? = null
-    private val hideToolWindowButton: JButton? = null
-    private val currentDate: JLabel? = null
-    private val currentTime: JLabel? = null
-    private val timeZone: JLabel? = null
-    val content: JPanel? = null
+    private val refreshToolWindowButton: JButton = JButton()
+    private val hideToolWindowButton: JButton = JButton()
+    private val currentDate: JLabel = JLabel()
+    private val currentTime: JLabel = JLabel()
+    private val timeZone: JLabel = JLabel()
+    val content: JPanel = JPanel()
 
     fun currentDateTime() {
         // Get current date and time
         val instance = Calendar.getInstance()
-        currentDate!!.text = (instance[Calendar.DAY_OF_MONTH].toString() + "/"
+        currentDate.text = (instance[Calendar.DAY_OF_MONTH].toString() + "/"
                 + (instance[Calendar.MONTH] + 1) + "/"
                 + instance[Calendar.YEAR])
         currentDate.icon = ImageIcon(javaClass.getResource("/toolWindow/Calendar-icon.png"))
         val min = instance[Calendar.MINUTE]
         val strMin = if (min < 10) "0$min" else min.toString()
-        currentTime!!.text = instance[Calendar.HOUR_OF_DAY].toString() + ":" + strMin
+        currentTime.text = instance[Calendar.HOUR_OF_DAY].toString() + ":" + strMin
         currentTime.icon = ImageIcon(javaClass.getResource("/toolWindow/Time-icon.png"))
         // Get time zone
         val gmt_Offset = instance[Calendar.ZONE_OFFSET].toLong() // offset from GMT in milliseconds
         var str_gmt_Offset = (gmt_Offset / 3600000).toString()
         str_gmt_Offset = if (gmt_Offset > 0) "GMT + $str_gmt_Offset" else "GMT - $str_gmt_Offset"
-        timeZone!!.text = str_gmt_Offset
+        timeZone.text = str_gmt_Offset
         timeZone.icon = ImageIcon(javaClass.getResource("/toolWindow/Time-zone-icon.png"))
     }
 
     init {
-        hideToolWindowButton!!.addActionListener { e: ActionEvent? -> toolWindow.hide(null) }
-        refreshToolWindowButton!!.addActionListener { e: ActionEvent? -> currentDateTime() }
+        visualPaddingsPanel()
+        hideToolWindowButton.addActionListener { e: ActionEvent -> toolWindow.hide(null) }
+        refreshToolWindowButton.addActionListener { e: ActionEvent -> currentDateTime() }
         currentDateTime()
     }
 }
