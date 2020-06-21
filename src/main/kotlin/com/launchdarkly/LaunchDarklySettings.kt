2b529e8dfc962feecdf32cdad19e3356bd07c281
Dan O'Brien <dobrien@launchdarkly.com>
2020-06-21 11:46:50 -0400

    show error

--- src/main/kotlin/com/launchdarkly/LaunchDarklySettings.kt
+++ src/main/kotlin/com/launchdarkly/LaunchDarklySettings.kt
@@ -1,35 +1,13 @@
 package com.launchdarkly
 
-import com.intellij.ide.AppLifecycleListener
-import com.intellij.ide.plugins.DynamicPluginListener
-import com.intellij.ide.plugins.IdeaPluginDescriptor
-import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
-import com.intellij.openapi.keymap.Keymap
-import com.intellij.openapi.keymap.KeymapManager
-import com.intellij.openapi.keymap.ex.KeymapManagerEx
 import com.intellij.openapi.options.BoundConfigurable
-import com.intellij.openapi.options.SearchableConfigurable
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.ComboBox
 import com.intellij.openapi.ui.DialogPanel
-import com.intellij.ui.SimpleListCellRenderer
 import com.intellij.ui.layout.panel
-import com.intellij.util.ui.FormBuilder
-import com.intellij.util.ui.UIUtil
-import com.intellij.util.xmlb.XmlSerializerUtil
-import java.awt.BorderLayout
-import java.awt.FlowLayout
-import java.awt.TextField
 import javax.swing.*
-
-//class LaunchDarklySettings {
-//    var project: String = ""
-//    var environment = ""
-//}
 
 @State(name = "LaunchDarklyConfig", storages = arrayOf(Storage(file = "launchdarkly.xml")))
 class LaunchDarklyConfig : PersistentStateComponent<LaunchDarklyConfig.State> {
@@ -52,7 +30,7 @@ class LaunchDarklyConfig : PersistentStateComponent<LaunchDarklyConfig.State> {
 
 class LaunchDarklyConfigurable() : BoundConfigurable(displayName = "LaunchDarkly Plugin") {
     private val settings = ServiceManager.getService(LaunchDarklyConfig::class.java)
-    private val project = JTextField("")
+    private val project = JTextField(settings.settings.project)
 
     override fun getHelpTopic() = null
 
